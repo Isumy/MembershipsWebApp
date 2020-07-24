8ac2016b0bb664d34ae576ab97b91ffae408c996
Isumy Fine <isu8907@gmail.com>
2020-07-23 22:20:46 -0400

    First Commit. New Images Folder

---
+++ MembershipsWebApp/MembershipsWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MembershipsWebApp.Startup))]
+namespace MembershipsWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
