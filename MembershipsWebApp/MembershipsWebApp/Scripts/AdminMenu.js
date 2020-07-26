25ade235dbdba001cd6eaec8c020dda9d006c698
Isumy Fine <isu8907@gmail.com>
2020-07-26 18:41:31 -0400

    Added AdminMenu.js

---
+++ MembershipsWebApp/MembershipsWebApp/Scripts/AdminMenu.js
@@ -0,0 +1,9 @@
+﻿//Edited
+//Document ready function
+//targeting the data-admin-menu attribute from the Admin View.
+//Funtion to open and close admin menu with dropdown list of items.
+$(function () {
+    $('[data-admin-menu]').hover(function(){
+        $('[data-admin-menu]').toggleClass('open'); 
+    });
+});
